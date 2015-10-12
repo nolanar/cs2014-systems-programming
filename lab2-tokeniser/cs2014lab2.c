65f5594c16f720d5faf9a6befae9e066b27a954c
Aran Nolan <nolanar@tcd.ie>
2015-10-12 23:03:03 +0100

    Final submitted version.

--- lab2-tokeniser/cs2014lab2.c
+++ lab2-tokeniser/cs2014lab2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 const int MAX_STRING = 256;
-const char DELIMITER = ' ';
 
 /* Find next token starting at give index.
  *   str : string to read from
@@ -11,19 +11,26 @@ const char DELIMITER = ' ';
  */
 int tokenise(char *str, int start, char *result);
 
-int main()
-{
-    char line[] = "Nothing but the rain  ";
+/* True if one of the following whitespace characters:
+ * (' ', '\n', '\t')
+ * False otherwise.
+ */
+bool isWhitespace(char ch);
+
+int main() {
+    printf("Enter string to be tokenised:\n");
+    char buffer[MAX_STRING];
+    fgets(buffer, MAX_STRING, stdin);
+
     char result[MAX_STRING];
     int start;
-
-    start = tokenise(line, 0, result);
+    start = tokenise(buffer, 0, result);
     while ( start != -1 ) {
         printf("%s\n", result);
-        start = tokenise(line, start, result);
+        start = tokenise(buffer, start, result);
     }
     if (result[0] != '\0') {
-        printf("%s\n", result);    
+        printf("%s\n", result);
     }
     return 0;
 }
@@ -31,13 +38,13 @@ int main()
 int tokenise(char *str, int start, char *result)
 {
     char ch = str[start];
-    while (ch == DELIMITER && ch != '\0')
+    while (ch != '\0' && isWhitespace(ch))
     {
         start++;
         ch = str[start];
     }
     int i = 0;
-    while (ch != '\0' && ch != DELIMITER)
+    while (ch != '\0' && !isWhitespace(ch))
     {
         result[i] = ch;
         i++;
@@ -45,4 +52,9 @@ int tokenise(char *str, int start, char *result)
     }
     result[i] = '\0';
     return ch == '\0' ? -1 : start + i;
+}
+
+bool isWhitespace(char ch)
+{
+    return ch == ' ' || ch == '\n' || ch == '\t';
 }
