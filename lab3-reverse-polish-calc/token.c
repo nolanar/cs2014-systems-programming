6a3c4e8abc83f1acaef5c1d48c54f1738a12710b
Aran Nolan <nolanar@tcd.ie>
2015-10-17 23:16:18 +0100

    Brace style reformatting.

--- lab3-reverse-polish-calc/token.c
+++ lab3-reverse-polish-calc/token.c
@@ -4,14 +4,12 @@
 int tokenise(char *str, int start, char *result)
 {
     char ch = str[start];
-    while (ch != '\0' && isspace(ch))
-    {
+    while (ch != '\0' && isspace(ch)) {
         start++;
         ch = str[start];
     }
     int i = 0;
-    while (ch != '\0' && !isspace(ch))
-    {
+    while (ch != '\0' && !isspace(ch)) {
         result[i] = ch;
         i++;
         ch = str[start + i];
