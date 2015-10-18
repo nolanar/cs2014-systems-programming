f1b9b9f296625e4cffa1539072b0a75abfb39006
Aran Nolan <nolanar@tcd.ie>
2015-10-18 19:18:45 +0100

    Refactor stack_t as stack_type

--- lab3-reverse-polish-calc/stack.c
+++ lab3-reverse-polish-calc/stack.c
@@ -2,26 +2,26 @@
 #include <assert.h>
 #include "stack.h"
 
-typedef struct node {
+struct node {
     double data;
     struct node *next;
-} *node_t;
+};
 
 struct stack {
-    node_t top;
+    struct node *top;
     size_t size;
 };
 
-stack_t stack_new()
+stack_type stack_new()
 {
-    stack_t stack = (stack_t) malloc(sizeof(struct stack));
+    stack_type stack = (stack_type) malloc(sizeof(struct stack));
     stack->top = NULL;
     stack->size = 0;
 
     return stack;
 }
 
-void stack_free(stack_t stack)
+void stack_free(stack_type stack)
 {
     while (!stack_isempty(stack)) {
         stack_pop(stack);
@@ -29,28 +29,28 @@ void stack_free(stack_t stack)
     free(stack);
 }
 
-void stack_push(stack_t stack, double value)
+void stack_push(stack_type stack, double value)
 {
-    // Create new node
-    node_t top_new = (node_t) malloc(sizeof(struct node));
+    /* Create new node */
+    struct node *top_new = (struct node *) malloc(sizeof(struct node));
     top_new->data = value;
     top_new->next = stack->top;
 
-    // Point top to new node
+    /* Point top to new node */
     stack->top = top_new;
     stack->size++;
 }
 
-double stack_pop(stack_t stack)
+double stack_pop(stack_type stack)
 {
-    // Precondition: stack must not be empty
+    /* Precondition: stack must not be empty */
     assert(!stack_isempty(stack));
 
-    // Get value of top element
-    node_t top_old = stack->top;
+    /* Get value of top element */
+    struct node *top_old = stack->top;
     double value = top_old->data;
 
-    // Point top to next in list
+    /* Point top to next in list */
     stack->top = top_old->next;
     stack->size--;
 
@@ -58,12 +58,12 @@ double stack_pop(stack_t stack)
     return value;
 }
 
-_Bool stack_isempty(stack_t stack)
+_Bool stack_isempty(stack_type stack)
 {
     return stack->top == NULL;
 }
 
-size_t stack_size(stack_t stack)
+size_t stack_size(stack_type stack)
 {
     return stack->size;
 }
--- lab3-reverse-polish-calc/stack.h
+++ lab3-reverse-polish-calc/stack.h
@@ -7,35 +7,35 @@
  *
  *  Node values are of type double.
  */
-typedef struct stack *stack_t;
+typedef struct stack *stack_type;
 
 /** Returns a new empty stack.
  *
  *  note: This should be used when a new stack is required.
  */
-stack_t stack_new();
+stack_type stack_new();
 
 /** Destroys stack.
  */
-void stack_free(stack_t stack);
+void stack_free(stack_type stack);
 
 /** Puts new double valued element on top of stack.
  */
-void stack_push(stack_t stack, double value);
+void stack_push(stack_type stack, double value);
 
 /** Removes top element from stack and returns it's value.
  *
  *  Precondition:
  *  The stack must not be empty.
  */
-double stack_pop(stack_t stack);
+double stack_pop(stack_type stack);
 
 /** Returns: true if stack is empty, false otherwise.
  */
-_Bool stack_isempty(stack_t stack);
+_Bool stack_isempty(stack_type stack);
 
 /** Returns the number of elements on the stack.
  */
-size_t stack_size(stack_t stack);
+size_t stack_size(stack_type stack);
 
 #endif
