d5c22769312008abbed88a1294063cd47a8777f5
Aran Nolan <nolanar@tcd.ie>
2015-10-18 02:47:50 +0100

    Let evaluate() return error states instead of program crashing

--- lab3-reverse-polish-calc/calc.c
+++ lab3-reverse-polish-calc/calc.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include "token.h"
 #include "stack.h"
 
 #define MAX_STRING 256
 
+/** Evaluates an expression in reverse polish notation.
+ *
+ *  Parameters:
+ *  char expression[] = expression to be evaluated
+ *
+ *  Return:
+ *  result      if expression is well formed
+ *  NAN         if expression is not well formed
+ *  INFINITY    if division by 0 occures
+ */
 double evaluate(char *expression);
+
 _Bool isoperator(char *token);
 
 int main()
@@ -27,15 +39,17 @@ int main()
 
 double evaluate(char *expression)
 {
-    char token[MAX_STRING];
     stack_t stack = stack_new();
+
+    char token[MAX_STRING];
     int index = tokenise(expression, 0, token);
     while (index != -1) {
         double value;
-        int isdouble = sscanf(token, "%lf", &value);
-        if (isdouble) {
+        char invalid = '\0';
+        int isdouble = sscanf(token, "%lf %c", &value, &invalid);
+        if (isdouble && !invalid) {
             stack_push(stack, value);
-        } else if (isoperator(token)) {
+        } else if (isoperator(token) && stack_size(stack) > 1) {
             double a = stack_pop(stack);
             double b = stack_pop(stack);
             char ch = token[0];
@@ -46,10 +60,18 @@ double evaluate(char *expression)
                 ch == '/' ? a / b:
                 0;
             stack_push(stack, result);
+        } else {
+            return NAN;
         }
         index = tokenise(expression, index, token);
     }
-    double result = stack_pop(stack);
+
+    double result;
+    if (stack_size(stack) == 1) {
+        result = stack_pop(stack);
+    } else {
+        result = NAN;
+    }
     stack_free(stack);
     return result;
 }
@@ -57,5 +79,6 @@ double evaluate(char *expression)
 _Bool isoperator(char *token)
 {
     char ch = token[0];
-    return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    _Bool isopchar = ch == '+' || ch == '-'|| ch == '*' || ch == '/';
+    return token[1] == '\0' && isopchar;
 }
